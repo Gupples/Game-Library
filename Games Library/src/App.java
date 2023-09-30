4e96ecdf3fc1c87d15ef71b09fd43f1a9439e67c
Gupples <85248061+Gupples@users.noreply.github.com>
2023-09-29 18:15:56 -0600

    Progress made on overall structure of games.

--- Games Library/src/App.java
+++ Games Library/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean isPlaying = true;
+        Scanner myInput = new Scanner(System.in);
+        while (isPlaying) {
+            // playNumbers();
+            System.out.println("Do you want to play again? y/n"); // prompt
+            /* Convert the next block of code to stand-alone function 
+            getStringInput(String prompt, String error). */
+            String playingInput = myInput.nextLine().toLowerCase(null);
+            if (playingInput == "n") {
+
+            }
+            try{
+            }
+            catch (Exception e) {
+                System.out.println("That doesn't match the bool input.");
+            }
+        } // exit while (isPlaying)
     }
+
+    public static void playNumbers() {
+        boolean isPlayNumbers = true;
+        while (isPlayNumbers) {
+
+        } // exit while (isPlayNumbers).
+
+    } // exit playNumbers()
+
+    public static void playHangman() {
+
+    } // exit playHangman
 }
