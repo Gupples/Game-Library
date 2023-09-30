a80493e16f503a4bdc875c8d8a5a047af6f832ba
Gupples <85248061+Gupples@users.noreply.github.com>
2023-09-29 18:37:06 -0600

    Made a lot of progress on playNumbers()

--- Games Library/src/App.java
+++ Games Library/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,6 +8,7 @@ public class App {
         while (isPlaying) {
             // playNumbers();
             System.out.println("Do you want to play again? y/n"); // prompt
+
             /* Convert the next block of code to stand-alone function 
             getStringInput(String prompt, String error). */
             String playingInput = myInput.nextLine().toLowerCase(null);
@@ -22,8 +24,33 @@ public class App {
     }
 
     public static void playNumbers() {
+        Scanner numbersScanner = new Scanner(System.in);
         boolean isPlayNumbers = true;
         while (isPlayNumbers) {
+
+            boolean gameOver = false;
+            while (gameOver == false){
+                ArrayList guesses = new ArrayList();
+                // Get game ceiling.
+                System.out.println("Enter game difficulty.");
+                int max = numbersScanner.nextInt();
+                int randomNumber = (int)(Math.random() * max + 1);
+                System.out.println("Guess a random number between 0 and " + max);
+                int numberGuess = numbersScanner.nextInt();
+                guesses.add(numberGuess);
+                if (numberGuess == randomNumber) {
+                    gameOver = true;
+                }
+                else if (numberGuess > randomNumber) {
+                    System.out.println("Too high.");
+                }
+                else {
+                    System.out.println("Too low.");
+                }
+                
+            } // exit while (gameOver == false)
+
+            
 
         } // exit while (isPlayNumbers).
 
